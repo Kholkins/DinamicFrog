7dba8f4d3067e82b1703f80b57a99425879a0551
Kholkin <kholkins@mail.ru>
2020-03-10 20:10:23 +0300

    create layout

--- app/src/main/java/com/example/dinamicfragment/MainActivity.java
+++ app/src/main/java/com/example/dinamicfragment/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.dinamicfragment;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
